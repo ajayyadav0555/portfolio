28a206b49d93ead34b049b0ccb80b04cd6b606ab
Ajay Yadav <ay3224587@gmail.com>
2025-04-17 17:26:57 +0530

    final commit

--- src/sections/ExperienceSection.jsx
+++ src/sections/ExperienceSection.jsx
@@ -74,7 +74,7 @@ const ExperienceSection = () => {
                                 <div className="xl:w-2/6">
                                     <GlowCard card={card}>
                                         <div>
-                                            <img src={card.imgPath} alt="exp-img" />
+                                            <img src={card.imgPath} alt="exp-img" className='w-42 h-20 rounded-3xl px-2 py-2'/>
                                         </div>
                                     </GlowCard>
                                 </div>
@@ -86,7 +86,7 @@ const ExperienceSection = () => {
                                         </div>
                                         <div className="expText flex xl:gap-20 md:gap-10 gap-5 relative z-20">
                                             <div className="timeline-logo">
-                                                <img src={card.logoPath} alt="logo" />
+                                                <img src={card.logoPath} alt="logo" className='rounded-3xl'/>
                                             </div>
                                             <div>
                                                 <h1 className="font-semibold text-3xl">{card.title}</h1>
